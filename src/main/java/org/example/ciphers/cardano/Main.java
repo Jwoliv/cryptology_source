c59c8a9d260a9a500b05fc39a0874834ce849be7
Jwoliv <bohdanlishchuk0@gmail.com>
2024-03-23 19:50:32 +0200

    [lab#4]

--- src/main/java/org/example/ciphers/cardano/Main.java
+++ src/main/java/org/example/ciphers/cardano/Main.java
@@ -1,7 +1,5 @@
 package org.example.ciphers.cardano;
 
-import org.example.ciphers.simple_permutation.SimplePermutation;
-
 import static org.example.utils.MsgText.PLAIN_TEXT_ENG_WITHOUT_SPACES_AND_EXTRA_SYMBOL;
 
 public class Main {
