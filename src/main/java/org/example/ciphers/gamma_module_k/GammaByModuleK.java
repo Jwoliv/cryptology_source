3a4eac6f42cdbef27dd4683a6a0f587a5c474fde
Lishchuk Bohdan <121406264+Jwoliv@users.noreply.github.com>
2024-04-07 14:25:45 +0300

    [lab#5]

--- src/main/java/org/example/ciphers/gamma_module_k/GammaByModuleK.java
+++ src/main/java/org/example/ciphers/gamma_module_k/GammaByModuleK.java
@@ -6,38 +6,38 @@ public class GammaByModuleK {
     private static final Integer ALPHABET_LENGTH = ALPHABET.length();
 
     public String encrypt(String plainText, String key) {
-        StringBuilder fullKey = new StringBuilder();
-        do {
-            fullKey.append(key);
-        } while (plainText.length() > fullKey.length());
-
-        String fullKeyStr = fullKey.substring(0, plainText.length());
+        String fullKeyStr = generateFullKey(plainText, key);
         StringBuilder encryptedText = new StringBuilder();
-
         for (int i = 0; i < plainText.length(); i++) {
             int indexText = ALPHABET.indexOf(plainText.charAt(i));
             int indexKey = ALPHABET.indexOf(fullKeyStr.charAt(i));
-            String newChar = String.valueOf(ALPHABET.charAt((indexText + indexKey) % ALPHABET_LENGTH));
-            encryptedText.append(newChar);
+            addSymbolToResult(indexText + indexKey, encryptedText);
         }
         return encryptedText.toString();
     }
 
     public String decrypt(String encryptedText, String key) {
-        StringBuilder fullKey = new StringBuilder();
-        do {
-            fullKey.append(key);
-        } while (encryptedText.length() > fullKey.length());
-
-        String fullKeyStr = fullKey.substring(0, encryptedText.length());
+        String fullKeyStr = generateFullKey(encryptedText, key);
         StringBuilder plainText = new StringBuilder();
-
         for (int i = 0; i < encryptedText.length(); i++) {
             int indexText = ALPHABET.indexOf(encryptedText.charAt(i));
             int indexKey = ALPHABET.indexOf(fullKeyStr.charAt(i));
-            String newChar = String.valueOf(ALPHABET.charAt((indexText - indexKey + ALPHABET_LENGTH) % ALPHABET_LENGTH));
-            plainText.append(newChar);
+            addSymbolToResult(indexText - indexKey + ALPHABET_LENGTH, plainText);
         }
         return plainText.toString();
+    }
+
+    private void addSymbolToResult(int index, StringBuilder plainText) {
+        String newChar = String.valueOf(ALPHABET.charAt(index % ALPHABET_LENGTH));
+        plainText.append(newChar);
+    }
+
+    private String generateFullKey(String plainText, String key) {
+        StringBuilder fullKey = new StringBuilder();
+        do {
+            fullKey.append(key);
+        } while (plainText.length() > fullKey.length());
+
+        return fullKey.substring(0, plainText.length());
     }
 }
