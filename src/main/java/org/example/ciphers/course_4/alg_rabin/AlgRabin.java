82383faa662edfcd16af9fbab911a17d6001b22b
Jwoliv <bohdanlishchuk0@gmail.com>
2024-09-14 18:03:48 +0300

    lab#3

---
+++ src/main/java/org/example/ciphers/course_4/alg_rabin/AlgRabin.java
@@ -0,0 +1,50 @@
+package org.example.ciphers.course_4.alg_rabin;
+
+public class AlgRabin {
+    public static final Long p = 239L;
+    public static final Long q = 19L;
+    public static final Long E = 2L;
+    public static final Long M = 10L;
+
+
+    public void proceed() {
+        Long n = p * q;
+        Long C = (long) Math.pow(M, 2) % n;
+        Long k = (p + 1) / 4;
+        Long l = (q + 1) / 4;
+        Long x = modExp(C, k, p);
+        Long y = modExp(C, l, p);
+
+        Long xm1 = x % p;
+        Long ym1 = y % p;
+
+        Long xm2 = x % p;
+        Long ym2 = -y % p;
+
+        Long xm3 = -x % p;
+        Long ym3 = y % p;
+
+        Long xm4 = -x % p;
+        Long ym4 = -y % p;
+
+        System.out.printf("m = %d\n", M);
+        System.out.printf("x = %d, y = %d\n", x, y);
+        System.out.printf("m1 = %d m1 = %d\n", xm1, ym1);
+        System.out.printf("m2 = %d m2 = %d\n", xm2, ym2);
+        System.out.printf("m3 = %d m3 = %d\n", xm3, ym3);
+        System.out.printf("m4 = %d m4 = %d\n", xm4, ym4);
+    }
+
+    public static long modExp(Long base, Long exp, Long mod) {
+        Long result = 1L;
+        base %= mod;
+        while (exp > 0) {
+            if ((exp & 1) == 1) {
+                result = (result * base) % mod;
+            }
+            exp = exp >> 1;
+            base = (base * base) % mod;
+        }
+        return result;
+    }
+}
---
+++ src/main/java/org/example/ciphers/course_4/alg_rabin/Main.java
@@ -0,0 +1,8 @@
+package org.example.ciphers.course_4.alg_rabin;
+
+public class Main {
+    public static void main(String[] args) {
+        AlgRabin algRabin = new AlgRabin();
+        algRabin.proceed();
+    }
+}
