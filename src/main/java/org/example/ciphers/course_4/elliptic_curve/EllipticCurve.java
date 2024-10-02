8757c6850752805a36f714df6828db8dabe3251c
Jwoliv <bohdanlishchuk0@gmail.com>
2024-10-02 19:34:14 +0300

    lab#4

--- src/main/java/org/example/ciphers/course_4/elliptic_curve/EllipticCurve.java
+++ src/main/java/org/example/ciphers/course_4/elliptic_curve/EllipticCurve.java
@@ -3,6 +3,7 @@ package org.example.ciphers.course_4.elliptic_curve;
 import lombok.*;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class EllipticCurve {
@@ -14,65 +15,78 @@ public class EllipticCurve {
     private static final Point G = new Point(0L, 1L);
     private static final Long kA = 2L;
     private static final Long kB = 11L;
-    private static final Long r = 13L;
-    private static Point pM;
+    private static List<Point> pMs = new ArrayList<>();
     private static Point yA;
     private static Point yB;
 
+    public EllipticCurve() {
+        List<Integer> asciiChairs = Arrays.stream(M.split("")).map(x -> (int) x.charAt(0)).toList();
+        pMs = asciiChairs.stream().map(G::multiply).toList();
+        yA = G.multiply(kA);
+        yB = G.multiply(kB);
+        System.out.println("list of the pm");
+        pMs.forEach(System.out::println);
+    }
 
-    public static void proceed() {
-        char[] chars = M.toCharArray();
-        List<Integer> asciiChars = new ArrayList<>();
-        for (var item: chars) {
-            asciiChars.add((int) item);
+
+    public List<PairPoint> encrypt(Integer r) {
+        Point rG = G.multiply(r);
+        List<Point> rs = pMs.stream().map(pm -> yB.multiply(r).add(pm)).toList();
+        return rs.stream().map(rso -> PairPoint.builder().first(rG).second(rso).build()).toList();
+    }
+
+    public List<Point> decrypt(List<PairPoint> pairPoints) {
+        List<Point> result = new ArrayList<>();
+        for (PairPoint pairPoint : pairPoints) {
+            Point rG = pairPoint.getFirst();
+            Point R = pairPoint.getSecond();
+            result.add(R.reduce(rG.multiply(kB)));
         }
-        pM = G.multiplyBy(r);
-        System.out.println("Init pm = " + pM);
-        yA= G.multiplyBy(kA);
-        yB = G.multiplyBy(kB);
-    }
-
-
-    public static PointPair encrypt(Long r) {
-        Point rG = G.multiplyBy(r);
-        Point rYb = yB.multiplyBy(r);
-        Point R = rYb.addPointers(pM);
-        return new PointPair(rG, R);
-    }
-
-    public static void decrypt(PointPair pair) {
-        Point kbrG = new Point(pair.rG.x * kB, pair.rG.y * kB);
-        Point pM = pair.R.addPointers(new Point(kbrG.x, kbrG.y * -1));
-        System.out.println("Decrypted pM = " + pM);
+        return result;
     }
 
 
     @Data
     @Builder
-    @ToString
     @NoArgsConstructor
     @AllArgsConstructor
     public static class Point {
         private Long x;
         private Long y;
 
-        public Point multiplyBy(Long r) {
+        public Point multiply(Integer r) {
             return new Point(x * r, y * r);
         }
 
-        public Point addPointers(Point p) {
-            return new Point(x + p.x, y + p.y);
+        public Point multiply(Long r) {
+            return new Point(x * r, y * r);
+        }
+
+        public Point add(Point pm) {
+            return new Point(x + pm.x, y + pm.y);
+        }
+
+        public Point reduce(Point pm) {
+            return new Point(x - pm.x, y - pm.y);
+        }
+
+        @Override
+        public String toString() {
+            return "{ %d, %d }".formatted(x, y);
         }
     }
 
     @Data
     @Builder
-    @ToString
     @NoArgsConstructor
     @AllArgsConstructor
-    public static class PointPair {
-        private Point rG;
-        private Point R;
-    }
+    public static class PairPoint {
+        private Point first;
+        private Point second;
 
+        @Override
+        public String toString() {
+            return "{ %d, %d } { %d, %d }".formatted(first.getX(), first.getY(), second.getX(), second.getY());
+        }
+    }
 }
